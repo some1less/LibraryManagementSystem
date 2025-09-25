a15047f301b785b6486749f0348296c3b53fef99
some1less <muracks.max@gmail.com>
2025-09-25 17:44:07 +0200

    feat: added rest of CRUD methods, needs fixing, todo specified inside

--- src/main/java/some1less/librarymanagsys/controller/UserController.java
+++ src/main/java/some1less/librarymanagsys/controller/UserController.java
@@ -1,8 +1,6 @@
 package some1less.librarymanagsys.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import some1less.librarymanagsys.exception.UserNotFoundException;
 import some1less.librarymanagsys.model.User;
 import some1less.librarymanagsys.repository.UserRepository;
@@ -27,4 +25,37 @@ public class UserController {
         return userRepository.findById(id)
                 .orElseThrow(() -> new UserNotFoundException(id));
     }
+
+    @PostMapping("/users")
+    User newUser(@RequestBody User newUser){
+
+        // todo: create rule for checking ID while creating
+        // todo: create auto-incrementing while creating
+
+        return userRepository.save(newUser);
+    }
+
+    @PutMapping("/users/{id}")
+    User modifyUser(@RequestBody User newUser, @PathVariable int id){
+
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+                    user.setPhoneNumber(newUser.getPhoneNumber());
+                    user.setAddress(newUser.getAddress());
+                    return userRepository.save(user);
+                }).orElseGet(() -> {
+                    return userRepository.save(newUser);
+                });
+    }
+
+    @DeleteMapping("/users/{id}")
+    void deleteUser(@PathVariable int id){
+
+        // todo: create other classes that will allow to create Lists for cascade deleting
+        userRepository.deleteById(id);
+    }
+
+
 }
